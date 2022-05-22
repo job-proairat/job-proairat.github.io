0c7a786baae9c26e1b562bd08c6474ef70479392
Shayakhmetov Airat <proairat@gmail.com>
2022-05-22 16:46:44 +0500

    remove "src" folder

--- src/components/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <SiteNavbarVue />
-    <h1>К сожалению, такой страницы не существует</h1>
-</template>
-
-<script setup lang="ts">
-import SiteNavbarVue from '@/components/SiteNavbar.vue';
-</script>
